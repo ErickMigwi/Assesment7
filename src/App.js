f1a9e3e3b9ba42be53353efad4a8c74a574228dd
ErickMigwi <muthigaerick@gmail.com>
2023-03-03 11:11:17 +0300

    add files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import Question1 from './Question1';
+import Question2 from './Question2';
 import Question3 from './Question3';
 import Question4 from './Question4';
 function App() {
   return (
     <div className="App">
+      <h1>Assement7</h1>
       <h1>Question1</h1>
      <Question1/>
+     <h1>Question2</h1>
+     <Question2 name=  {'Erick'}/>
      <h1>Qusttion3</h1>
      <Question3/>
      <h1>Qusttion4</h1>
--- src/Question2.jsx
+++ src/Question2.jsx
@@ -1,11 +1,38 @@
-class  extends Component {
-    constructor(props) {
-        super(props);
-    }
-    state = {  }
-    render() { 
-        return (  );
-    }
+import React from 'react';
+
+class Question2 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+    console.log('Constructor');
+  }
+
+  componentDidMount() {
+    console.log('Component Did Mount');
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log('Component Did Update');
+  }
+
+  componentWillUnmount() {
+    console.log('Component Will Unmount');
+  }
+
+  handleClick = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  render() {
+    console.log('Render');
+    return (
+      <div>
+        <h1>{this.props.title}</h1>
+        <p>Count: {this.state.count}</p>
+        <button onClick={this.handleClick}>Increment</button>
+      </div>
+    );
+  }
 }
- 
-export default ;
+
+export default Question2;
